fcecaddbd50c7c7de017c01609eece072fe4637e
timheckel <tim@slatebox.com>
2012-03-06 17:31:19 +0000

    "added easyimage"

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-alert('one');
+var easyimg = require('easyimage');
+easyimg.info('https://www.google.com/images/srpr/logo3w.png', function(err, stdout, stderr) {
+    if (err) throw err;
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end(stdout);      
+});
