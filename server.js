085625ecb0e345bcb2c3f49285231495425f5f0a
timheckel <tim@slatebox.com>
2012-03-06 18:21:42 +0000

    "updating server"

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-var easyimg = require('easyimage');
-
 var http = require('http');
+var easyimg = require('easyimage');
 http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/plain'});
    easyimg.info('https://www.google.com/images/srpr/logo3w.png', function(err, stdout, stderr) {
+       res.writeHead(200, {"Content-Type": "text/plain"});
         if (err) throw err;
-        res.writeHead(200, {"Content-Type": "text/plain"});
         res.end(stdout);
     });
 }).listen(process.env.PORT);
